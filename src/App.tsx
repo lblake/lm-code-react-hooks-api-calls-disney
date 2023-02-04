2f094ac7544a932564d86b62114a48e09b4f8ecb
lblake <treleven.lloyd@gmail.com>
2023-02-04 17:16:00 +0000

    replaced hardcoded disney character data with api call along with usig useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,45 @@
-
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
 import { DisneyCharacter } from './disney_character';
 
-const App : React.FC = () => {
+const App: React.FC = () => {
+  const baseURL = 'https://api.disneyapi.dev/';
 
-	const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    getCharacters(currentPage);
+  }, [currentPage]);
 
   // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
-  
+  const getCharacters = async (pageNumber: number) => {
+    try {
+      const getResponse = await fetch(
+        baseURL + `characters?page=${pageNumber}`
+      );
+      if (getResponse.ok) {
+        const json = (await getResponse.json()) as {
+          data: DisneyCharacter[];
+        };
+        setCharacters(json.data);
+      }
+    } catch (error) {
+      console.log('error');
+    }
+  };
 
   return (
-    <div className="page">
+    <div className='page'>
       <Header currentPage={currentPage} />
       <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
       <CharacterContainer characters={characters} />
     </div>
   );
-}
+};
 
 export default App;
